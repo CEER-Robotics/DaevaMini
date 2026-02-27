8c06d6393e0bd995e284100a53fe9ca3f3260ed4
Federico <federicomarocco5121999@gmail.com>
2026-02-27 11:13:07 +0100

    Aggiunta modalità cambio contenitori rossi, ma ancora da migliorare

---
+++ Views/RedContainerSetupPage.axaml.cs
@@ -0,0 +1,22 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace DaevaMini.Views;
+
+public partial class RedContainerSetupPage : UserControl
+{
+    public RedContainerSetupPage()
+    {
+        InitializeComponent();
+    }
+
+    public void OnBackArrow(object? sender, RoutedEventArgs e)
+    {
+        // Logica back
+    }
+
+    public void OnDone(object? sender, RoutedEventArgs e)
+    {
+        // Logica done
+    }
+}
