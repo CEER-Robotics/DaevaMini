65eda3cf53999c370c58a10daf1ea0238b5338da
Federico <federicomarocco5121999@gmail.com>
2026-03-06 22:39:40 +0100

    Inserimento settings daeva max

---
+++ SettingsDaevaMax.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DaevaMini;
+
+public partial class SettingsDaevaMax : UserControl
+{
+    public SettingsDaevaMax()
+    {
+        InitializeComponent();
+    }
+}
